bf4dd0a4a222a19ef053567ea1f2fac799abc5f5
alamin15-13174 <alamin15-13174@diu.edu.bd>
2022-06-11 10:21:45 -0700

    first commit

---
+++ CONDITION.py
@@ -0,0 +1,32 @@
+# if| else| elif
+
+if  100>10:
+    print("True")
+else:
+    print("False")
+
+
+#elif
+if  100 < 10:
+       print("100 is greater than 10")
+elif 100 < 20:
+           
+       print("100 is greater than 10")
+elif 100 > 20:
+           
+       print("10 is greater than 10")
+else:
+           
+       print("No one is true")
+# only check 1,2,3...... if find 3 is true then it didnt go next step and print it
+
+#nested loop
+if  100 > 10:
+    if 100 < 110:
+       print("outter condition is true")
+       if 100 <110:
+        print("inner condition is true")   
+       elif 100 < 20:
+           print("inner condition is true")
+else:
+ print("outter condition is False")
---
+++ loop.py
@@ -0,0 +1,16 @@
+#for,while
+
+#for i in range(10): #start=0,end=my,step=2
+    #print(i)
+
+
+#for i in range(2, 15, 3):
+ #print(i)
+
+ # while
+
+i = 0
+n = 10 
+while i<n:
+    print("gooon")
+    i += i + 1
